4b30fd64448d4a22e8307d6c828b007387dd5817
龙猫 <751942028@qq.com>
2018-09-25 16:26:21 +0800

    Initial commit

---
+++ src/main/java/com/wx/nr/NrApplication.java
@@ -0,0 +1,12 @@
+package com.wx.nr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NrApplication.class, args);
+    }
+}
